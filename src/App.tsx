d56ba5c098621c311d26a4da901611d0f2c45119
timbek2 <tim.beck46@gmail.com>
2025-07-17 15:26:14 -0400

    Add construction icons and text, remove vite and react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import constructionBanner from './assets/under_construction.gif'
+import constructionIcon from './assets/under-construction-90s.gif'
 import './App.css'
 
 function App() {
@@ -9,27 +9,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={constructionBanner} className="construction-banner" alt="Under Construction Banner" />
+        <img src={constructionIcon} className="construction-icon" alt="Under construction icon" />
+        <img src={constructionBanner} className="construction-banner" alt="Under Construction Banner" />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
+      <p className="coming-soon">
         Hello World
+      </p>
+      <p className="coming-soon">
+        Check back soon to see what I've become!
       </p>
     </>
   )
