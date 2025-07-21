403c6faea395904a9da76dd5c0702a360bb1617f
timbek2 <tim.beck46@gmail.com>
2025-07-21 15:37:51 -0400

    Add react-parallax-tilt, update some about copy

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -4,10 +4,15 @@ const About = () => {
   return (
     <div className="about">
       <h2>About Me</h2>
-      <p>Hi, I'm Tim. I'm a software engineer living in Brooklyn.</p>
+      <p>
+        Hi, I'm Tim. I'm a software engineer living in Brooklyn.
+      </p>
       <p>
         I love building things, most of the time with Javascript & Node, Ruby on Rails or wood.
-        I have a passion for creating user-facing applications that are both functional and visually appealing. I've worked extensively with ecommerce platforms and analytics, personally implementing a custom analytics solution for a large ecommerce site.
+        I have a passion for creating user-facing applications that are both functional and visually appealing. I've worked extensively with ecommerce platforms and analytics, and am always looking for new challenges to further develop my skills.
+      </p>
+      <p>
+        I'm always looking to work somewhere that values high quality code equally with collaboration and continuing education and development of its employees. I love working on challenging problems that require creative solutions, and I think that we do our best work when we collaborate with others and share our knowledge. Siloing information is a problem that only compounds over time.
       </p>
       <p>
         I've also built a few cutting boards and a coffee table, with some more ideas that are probably too ambitious.
