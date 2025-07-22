27561d4ad152ab752310ac04633e212f99c405f6
timbek2 <tim.beck46@gmail.com>
2025-07-22 19:59:04 -0400

    Add vertical timeline element library, add some styles, create constants folder to hold things like experience

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,10 +1,31 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css';
+import shiftLab from '../../assets/shift-lab-logo.svg';
 import './style.css';
 
 const Experience = () => {
   return (
     <section className="experience">
       <h2>Experience</h2>
-      <p>Coming soon!</p>
+      <VerticalTimeline lineColor={'#bb9af7'}>
+        <VerticalTimelineElement
+          className="vertical-timeline-element-work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2011 - present"
+          iconStyle={{ background: 'none', color: '#fff', boxShadow: "0 0 0 4px #bb9af7" }}
+          icon={<img src={shiftLab} className="work-icon" alt="Shift Lab Logo" />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </section>
   );
 }
