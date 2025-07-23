e595e83d2c12acb0ab21470a6aa973552debaf00
timbek2 <tim.beck46@gmail.com>
2025-07-23 01:05:32 -0400

    Add experiences, fix juice logo, finish styling experiences, import constant from constants

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -3,8 +3,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
-import shiftLab from '../../assets/shift-lab-logo.svg';
-import hubbleContacts from '../../assets/hubble-logo.png';
+import experiences from '../../constants';
 import './style.css';
 
 const Experience = () => {
@@ -12,7 +11,26 @@ const Experience = () => {
     <section className="experience">
       <h2>Experience</h2>
       <VerticalTimeline lineColor={'#bb9af7'}>
-        <VerticalTimelineElement
+        {experiences.map((experience, index:number) => (
+          <VerticalTimelineElement
+            key={index}
+            className="vertical-timeline-element-work"
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            date={experience.date}
+            iconStyle={{ boxShadow: "0 0 0 4px #bb9af7" }}
+            icon={<img src={experience.icon} alt={`${experience.companyName} Logo`} className={`work-icon ${experience.iconClass}`} />}
+          >
+            <h3 className="vertical-timeline-element-title">{experience.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{experience.location}</h4>
+            <ul>
+              {experience.points.map((point, pointIndex) => (
+                <li key={pointIndex}>{point}</li>
+              ))}
+            </ul>
+          </VerticalTimelineElement>
+        ))}
+        {/* <VerticalTimelineElement
           className="vertical-timeline-element-work"
           contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
@@ -26,7 +44,7 @@ const Experience = () => {
           <p>
             Creative Direction, User Experience, Visual Design, Project Management, Team Leading
           </p>
-        </VerticalTimelineElement>
+        </VerticalTimelineElement> */}
       </VerticalTimeline>
     </section>
   );
