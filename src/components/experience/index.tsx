31ee1aff077637b00b20400ac499c0c4b4208057
timbek2 <tim.beck46@gmail.com>
2025-07-22 22:15:18 -0400

    Find props to control icon styles so I can work with circle and square icons. write boilerplate consts for experience content

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -4,6 +4,7 @@ import {
 } from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
 import shiftLab from '../../assets/shift-lab-logo.svg';
+import hubbleContacts from '../../assets/hubble-logo.png';
 import './style.css';
 
 const Experience = () => {
@@ -16,8 +17,9 @@ const Experience = () => {
           contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           date="2011 - present"
-          iconStyle={{ background: 'none', color: '#fff', boxShadow: "0 0 0 4px #bb9af7" }}
-          icon={<img src={shiftLab} className="work-icon" alt="Shift Lab Logo" />}
+          iconStyle={{ background: 'none', boxShadow: "0 0 0 4px #bb9af7" }}
+          icon={<img src={hubbleContacts} className="work-icon" alt="Shift Lab Logo" />}
+          iconClassName=""
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
           <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
