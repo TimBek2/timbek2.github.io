0ed0d7423e5857bfe7d4f1a17cfc766545659ff2
timbek2 <tim.beck46@gmail.com>
2025-08-05 17:14:12 -0400

    Smooth scroll links and fix top link to not add anything to url

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -3,7 +3,9 @@ import './style.css';
 const Navigation = () => {
   return (
     <nav className="navigation">
-      <h3 className="nav-title"><a className="nav-logo" href="#hero">TB</a></h3>
+      <h3 className="nav-title">
+        <a className="nav-logo" href={void(0)} onClick={() => window.scroll(0,1)}>TB</a>
+      </h3>
       <ul className='nav-links'>
         <li className='nav-link'><a href="#about">About Me</a></li>
         <li className='nav-link'><a href="#experience">Experience</a></li>
