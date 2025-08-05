fd34c724ec310021bb250c7b57b1d17e8f523733
timbek2 <tim.beck46@gmail.com>
2025-08-05 18:38:16 -0400

    Shrink animation for mobile phones so it doesnt overflow

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -7,7 +7,7 @@ const Navigation = () => {
         <a className="nav-logo" href={void(0)} onClick={() => window.scroll(0,1)}>TB</a>
       </h3>
       <ul className='nav-links'>
-        <li className='nav-link'><a href="#about">About Me</a></li>
+        <li className='nav-link'><a href="#about">About</a></li>
         <li className='nav-link'><a href="#experience">Experience</a></li>
         <li className='nav-link'><a href="#contact">Contact</a></li>
         <li className='nav-link'>
